d43dfa1b828e85f1a299e35b08cd97717b824561
Luiz Renato <luizrenato2005@gmail.com>
2018-09-16 18:45:54 -0300

    alteraçoes uso OAuth2

--- src/main/java/com/example/demo/security/ResourceServerConfig.java
+++ src/main/java/com/example/demo/security/ResourceServerConfig.java
@@ -1,40 +1,49 @@
 package com.example.demo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityWeb extends WebSecurityConfigurerAdapter {
+@EnableResourceServer
+public class ResourceServerConfig extends  ResourceServerConfigurerAdapter{
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	@Autowired
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth
 			.inMemoryAuthentication()
 			.withUser("admin").password("admin").roles("ROLE");
 	}
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
 //		autoriza requests 
 //        /categorias -> permite todos
 //        /qualquer request - autenticado e
 //        gerenciadoment sessao _ politica de criação de sessao -> stateless e
 //        csrf desabilitado
-
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
 			.antMatchers("/situacoes").permitAll()
 			.anyRequest().authenticated().and()
-			.httpBasic().and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 			.csrf().disable();
-			
 	}
+
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.stateless(true);
+	}
+	
+	
 
 	
 }
