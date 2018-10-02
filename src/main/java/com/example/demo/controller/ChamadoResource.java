825478142e3f3a465b9262806250869c06bba6bc
Luiz Renato <luizrenato2005@gmail.com>
2018-10-02 00:16:13 -0300

    incluido suporte a CORS

--- src/main/java/com/example/demo/controller/ChamadoResource.java
+++ src/main/java/com/example/demo/controller/ChamadoResource.java
@@ -4,19 +4,16 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.validation.Valid;
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.beans.Chamado;
@@ -27,6 +24,7 @@ import com.example.demo.service.ChamadoService;
 
 @RestController
 @RequestMapping("/api/chamados")
+@CrossOrigin
 public class ChamadoResource {
 	
 	@Autowired
